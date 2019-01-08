96928a7636680515084ced1003fe2b92dc4b2a90
ivanmeler <i_ivan@windowslive.com>
2019-01-08 11:19:17 +0100

    Remove outdated wifi firmwares

--- hero-common-vendor-blobs.mk
+++ hero-common-vendor-blobs.mk
@@ -193,10 +193,6 @@ PRODUCT_COPY_FILES += \
   vendor/samsung/hero-common/proprietary/vendor/etc/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf \
   vendor/samsung/hero-common/proprietary/vendor/firmware/bcm4359C0_V0147.0335_murata.hcd:system/vendor/firmware/bcm4359C0_V0147.0335_murata.hcd \
   vendor/samsung/hero-common/proprietary/vendor/firmware/bcm4359C0_V0147.0336_semco.hcd:system/vendor/firmware/bcm4359C0_V0147.0336_semco.hcd \
-  vendor/samsung/hero-common/proprietary/vendor/firmware/bcm4359C0_V0135.0265_murata.hcd:system/vendor/firmware/bcm4359C0_V0135.0265_murata.hcd \
-  vendor/samsung/hero-common/proprietary/vendor/firmware/bcm4359C0_V0135.0266_semco.hcd:system/vendor/firmware/bcm4359C0_V0135.0266_semco.hcd \
-  vendor/samsung/hero-common/proprietary/vendor/firmware/bcm4359C0_V0138.0278_murata.hcd:system/vendor/firmware/bcm4359C0_V0138.0278_murata.hcd \
-  vendor/samsung/hero-common/proprietary/vendor/firmware/bcm4359C0_V0138.0279_semco.hcd:system/vendor/firmware/bcm4359C0_V0138.0279_semco.hcd \
   vendor/samsung/hero-common/proprietary/vendor/firmware/companion_2l1_master_setfile.bin:system/vendor/firmware/companion_2l1_master_setfile.bin \
   vendor/samsung/hero-common/proprietary/vendor/firmware/companion_2l1_master_setfile_d.bin:system/vendor/firmware/companion_2l1_master_setfile_d.bin \
   vendor/samsung/hero-common/proprietary/vendor/firmware/companion_2l1_mode_setfile.bin:system/vendor/firmware/companion_2l1_mode_setfile.bin \
